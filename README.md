bd0779b8ef91807fdfd5cd9e2756b4ed024cea7b
ryhanlon <ryhanlon@gmail.com>
2018-11-27 20:17:44 -0800

    feat: Added messages, finished README, 'Play Again' button

--- README.md
+++ README.md
@@ -24,7 +24,8 @@ Click [here](#game-interface) to see pictures of the game interface.
 
 
 ##### Technical Components
-- JavaScript (ECMASript 6)
+- JavaScript (ECMAScript 6)
+- CSS and HTML
 
 
 ##### Articles and Tutorials
@@ -35,8 +36,8 @@ Used the following tutorials, articles and docs while building this app.
 
 - 2D Collision Detection [article](http://blog.sklambert.com/html5-canvas-game-2d-collision-detection#d-collision-detection)
 
-- Tutorial Request: FEND Project 3 - Service Workers [video](https://www.youtube.com/watch?v=2PY733qFR3A&feature=youtu.be).
-This video gave a nice review of Service Worker without a lot of additional files.
+- Tutorial Request: FEND Project 3 Arcade Game - Walk Through, by Ryan Waite [video](https://www.youtube.com/watch?v=XEVnMgYblGc&index=3&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s).
+This video helped with structuring some of the prototypes, helped me to see the components put together for the project, sample of Math.random
 
 - w3schools web docs
     - [Game Canvas](https://www.w3schools.com/graphics/game_canvas.asp)
@@ -46,12 +47,17 @@ This video gave a nice review of Service Worker without a lot of additional file
     - 2d Breakout Game Using Pure Javascript [intro docs](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript)
     - Set up wall boundries [see docs](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls)
 
+#### Future Features
+- Add a scoring system
+- Add sound efx
+- Add some 'Treasures' for additional points
+
 #### Game Interface
 
-![Game Screen 1](./docs/img/mobile_home-1.png)
+![Game Screen 1](./docs/imgs/fg_startGame.png)
 
-![Game Screen 2](./docs/img/mobile_home-2.png)
+![Game Screen 2](./docs/imgs/fg_winGame.png)
 
-![Game Screen 3](./docs/img/mobile_reviews-1.png)
+![Game Screen 3](./docs/imgs/fg_loseGame.png)
 
 
--- js/app.js
+++ js/app.js
@@ -4,40 +4,19 @@
  * @param {y} y position of a Enemybug
  * @param {speed} speed of a Enemybug
  */
-let Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances
+let Enemy = function (x, y, speed) {
+	// Variables applied to each of our instances
 
-    // The image/sprite for our enemies, this uses a helper from resources.js
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+	// The image/sprite for our enemies, this uses a helper from resources.js
+	this.sprite = 'images/enemy-bug.png';
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
 };
-
-// Collision details
-// Enemy.prototype.collision = function() {
-	// let enemyXRight = this.x + 50;
-	// let enemyXLeft = this.x - 50;
-	// let enemyYTop = this.y - 50;
-	// let enemyYBottom = this.y + 50;
-
-// 	if (object1.x < object2.x + object2.width  && object1.x + object1.width  > object2.x &&
-// 		object1.y < object2.y + object2.height && object1.y + object1.height > object2.y) {
-// // The objects are touching
-// }
-
-// 	if (player.x < enemyXRight && player.x > enemyXLeft &&
-// 		player.y > enemyYTop  && player.y < enemyYBottom) {
-// // The objects are touching
-// 		Player.resetPosition();
-
-// }
-//
-// };
 
 // Update the enemy's position
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
 	// Multiply any movement by the dt parameter
 	// which will ensure the game runs at the same speed for all computers
 
@@ -51,42 +30,41 @@ Enemy.prototype.update = function(dt) {
 	// Sides of enemy for collision
 	let enemyXRight = this.x + 65;
 	let enemyXLeft = this.x - 65;
-	let enemyYTop = this.y - 65;
 	let enemyYBottom = this.y + 65;
+	let enemyYTop = this.y - 65;
 
+	// Collision algorithm
 	if (player.x < enemyXRight && player.x > enemyXLeft &&
 		player.y > enemyYTop && player.y < enemyYBottom) {
-// The objects are touching
+		// The objects are touching
 		player.resetPosition();
 		player.loseMessage();
 
 	}
 };
 
-
 // Draw the enemy on the screen
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
 /**
  * @description  Player class, add update(), render() and handleInput() methods
  */
-let Player = function() {
-    this.sprite = 'images/char-boy.png';
+let Player = function () {
+	this.sprite = 'images/char-boy.png';
 	this.x = 303;
 	this.y = 404;
 	this.h_step = 101;
 	this.v_step = 83;
 };
 
-
-Player.prototype.winMessage = function() {
+Player.prototype.winMessage = function () {
+	// Message for winning the game
 	const winMessage = `Hooray!  You win!`;
 
 	const messageContainer = document.querySelector('.message-container');
-	// messageContainer.classList.remove('remove-message');
 	messageContainer.classList.add('show-message');
 
 	const messageHeading = document.querySelector('.message');
@@ -95,8 +73,9 @@ Player.prototype.winMessage = function() {
 
 };
 
-Player.prototype.loseMessage = function() {
-	const loseMessage = `Oh, drats.  The enemy-bug got you!`;
+Player.prototype.loseMessage = function () {
+	// Message for losing the game
+	const loseMessage = `Oh, dratts.  The enemy-bug got you!`;
 
 	const messageContainer = document.querySelector('.message-container');
 	// messageContainer.classList.remove('remove-message');
@@ -108,30 +87,29 @@ Player.prototype.loseMessage = function() {
 
 };
 
-Player.prototype.removeGameAlertLoser = function () {
+Player.prototype.gameAlert = function () {
+	// Remove the game alerts for win/lose
 	const message = document.querySelector('.message-container');
 	message.classList.remove('show-message');
-	message.classList.add('remove-message');
 };
 
-
-Player.prototype.removeButton = function() {
-	document.getElementById("playAgain").addEventListener("click", this.removeGameAlertLoser);
+Player.prototype.removeMessageDrawer = function () {
+	// Event listener for the "Play again" button on the game alert modal window
+	document.getElementById("playAgain").addEventListener("click", this.gameAlert);
 
 };
 
-
-// When game ends, put player back in starting position
-Player.prototype.resetPosition = function() {
+// When game ends, put player back in starting position, remove message modal window
+Player.prototype.resetPosition = function () {
 	this.winMessage();
-	this.removeButton();
+	this.removeMessageDrawer();
 	this.x = 303;
 	this.y = 404;
 };
 
 // Draw the player on the screen
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Player.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Keyboard controller with arrow keys
@@ -157,8 +135,8 @@ Player.prototype.handleInput = function (direction) {
 };
 
 // Objects instantiated
-// randomAtor of random speeds for enemy
-const randomAtor = () => (Math.floor(Math.random() * 500 + 1 / 3));
+// Random speeds for enemy
+const randomAtor = () => (Math.floor(Math.random() * 500 + 1 / 2));
 
 let enemyBug1 = new Enemy(-80, 60, randomAtor());
 let enemyBug2 = new Enemy(-80, 60 + 80, randomAtor());
@@ -172,15 +150,15 @@ let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    if (allowedKeys[e.keyCode]) {
+	if (allowedKeys[e.keyCode]) {
 		player.handleInput(allowedKeys[e.keyCode]);
 	}
 });
